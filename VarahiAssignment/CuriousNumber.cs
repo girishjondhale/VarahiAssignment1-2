1907bc3eca621b2ee24d2d239a01bd4b4c98330e
girishjondhale <136054330+girishjondhale@users.noreply.github.com>
2024-02-13 10:12:45 -0800

    Add project files.

---
+++ VarahiAssignment/CuriousNumber.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarahiAssignment
+{
+    internal class CuriousNumber
+    {
+        
+        static int Factorial(int n)
+        {
+                int result = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    result = result * i;
+                }
+                return result;
+            
+        }
+        static int FactorialOfDigits(int number)
+        {
+            int result = 0;
+
+            while (number > 0)
+            {
+                int Digit = number % 10;
+
+                int digitFactorial = Factorial(Digit);
+
+                result = result + digitFactorial;
+
+                number = number / 10;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = FactorialOfDigits(number);
+
+            Console.WriteLine($"The sum of factorial of digits is {result}");
+        }
+    }
+}
---
+++ VarahiAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace VarahiAssignment
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            HashSet<int> distinctTerms = new HashSet<int>();
+
+//            for (int a = 2; a <= 5; a++)
+//            {
+//                for (int b = 2; b <= 5; b++)
+//                {
+//                    int result = (int)Math.Pow(a, b);
+//                    distinctTerms.Add(result);
+//                }
+//            }
+
+//            // Convert HashSet to an array and sort it
+//            int[] sortedDistinctTerms = new int[distinctTerms.Count];
+//            distinctTerms.CopyTo(sortedDistinctTerms);
+//            Array.Sort(sortedDistinctTerms);
+
+//            // Print the sorted distinct terms
+//            Console.WriteLine("Sequence of distinct terms:");
+//            foreach (int term in sortedDistinctTerms)
+//            {
+//                Console.WriteLine(term);
+//            }
+//        }
+//    }
+//}
